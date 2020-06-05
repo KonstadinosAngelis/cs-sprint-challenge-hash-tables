a9dd766cc8cf5a4a70ea2e1b326fbae42e41e1fc
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-06-04 18:52:56 -0700

    finished 2,3, and 4. Working on getting the error's on 5 done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,7 +5,16 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    print(tickets.source)
+    cache = {}
 
-    result = ""
-    return result
+    for e in tickets:
+        cache[e.source] = e.destination
+    
+    pointer = cache["NONE"]
+    outputArr = [pointer]
+
+    while cache[pointer] != "NONE":
+        outputArr.append(cache[pointer])
+        pointer = cache[pointer]
+
+    return outputArr
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -3,19 +3,32 @@
 
 
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
 
-    return result
+    for idx, e in enumerate(files):
+        output = e.split("/")
+
+        if output[-1] in cache:
+            cache[output[-1]][1] += 1
+        else:
+            cache[output[-1]] = [idx, 1]
+
+    outputArr = []
+    for e in queries:
+        if e in cache:
+            # print(cache[e][1])
+            for t in range(cache[e][1]):
+                outputArr.append(files[cache[e][0]])
+    print(outputArr)
+    return outputArr
 
 
 if __name__ == "__main__":
     files = [
         '/bin/foo',
         '/bin/bar',
-        '/usr/bin/baz'
+        '/usr/bin/baz',
+        '/test/EF/foo'
     ]
     queries = [
         "foo",
